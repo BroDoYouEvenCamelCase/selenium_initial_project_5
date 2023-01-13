9a4b4c5544e46112ffde365a67cf828509adacac
Nafiye Demir <nafiyemdemir@hotmail.com>
2023-01-13 18:06:15 -0500

    added project

--- src/TestCase3.java
+++ src/TestCase3.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import utils.Driver;
+
+import java.util.List;
 
 public class TestCase3 {
     public static void main(String[] args) {
@@ -9,5 +13,14 @@ public class TestCase3 {
         driver.get("https://automationexercise.com/");
 
 
+        List<WebElement> elements = driver.findElements(By.cssSelector("div[class='single-widget"));
+        for (WebElement e : elements) {
+            if (driver.findElement(By.id("susbscribe_email")).isDisplayed())
+                System.out.println("email placeholder validation PASSED");
+            if (driver.findElement(By.cssSelector("i[class*='fa fa-arrow'")).isDisplayed())
+                System.out.println("arrow submit validation PASSED");
+            System.out.println(e.getText());
+
+        }
     }
 }
